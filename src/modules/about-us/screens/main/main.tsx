aadb2bbffab893cc67bce0cff56f776652973af7
Gabriel Henrique <p-gabriel.henrique@bbchain.com.br>
2025-02-07 16:45:23 -0300

    Add About Us section with leadership team and directors data

---
+++ src/modules/about-us/screens/main/main.tsx
@@ -0,0 +1,268 @@
+/* eslint-disable @next/next/no-img-element */
+'use client';
+
+import { directorsData, leadershipTeamData } from '@/utils/about-us/cards-data';
+import { Box, Container, Link, Typography, useMediaQuery, useTheme } from '@mui/material';
+import Image from 'next/image';
+import { CardsGrid } from './components/cards-grid/cards-grid';
+
+
+const fontFamilyBoldVar = 'var(--font-ample-soft-bold)';
+
+export function AboutUs() {
+  const theme = useTheme();
+  const isBiggerThanLarge = useMediaQuery(theme.breakpoints.up('xl'));
+  return (
+    <Box>
+      <Container maxWidth={isBiggerThanLarge ? 'xl' : 'lg'}>
+        <Box
+          position="absolute"
+          width={950}
+          height={650}
+          top={0}
+          right={0}
+          zIndex={-1}
+        >
+          <Image src="/assets/inicio-lateral.svg" alt="logo" fill />
+        </Box>
+        <Box
+          width="50%"
+          mt={8}
+        >
+          <Typography
+            variant="h3"
+            fontWeight={600}
+            mb={3}
+            fontFamily={fontFamilyBoldVar}
+          >
+            <Typography
+              component="span"
+              color="primary.main"
+              fontFamily={fontFamilyBoldVar}
+              variant="h3"
+              fontWeight={600}
+              mb={20}
+            >
+              Orchestrate the Universe
+            </Typography>
+          </Typography>
+          <Typography
+            variant="h5"
+            fontWeight={600}
+            mb={4}
+            fontFamily={'sans-serif'}
+          >
+            About Stonebranch
+          </Typography>
+          <Typography
+            variant="h6"
+            fontWeight={500}
+            mb={4}
+            fontFamily={'sans-serif'}
+          >
+            With decades of experience, Stonebranch set out to change the IT automation market for the better. To develop the right purpose-built solutions with user experience and accessibility in mind. To provide a true orchestration engine that empowers our customers to automate any on-prem or cloud solution. To ultimately enable you with the centralized control needed to better manage and scale with your expanding hybrid IT ecosystem.
+          </Typography>
+          <Typography
+            variant="h6"
+            fontWeight={500}
+            mb={4}
+            fontFamily={'sans-serif'}
+          >
+            Stonebranch is a force to be reckoned with in the IT orchestration and automation platform space. We shoot past the point where others have stopped, challenging the status quo, and exceeding expectations. We do this with the personalized service and leading-edge technology that is lacking with other providers.
+          </Typography>
+          <Typography variant="h5" fontWeight={500} mb={4} fontFamily={'sans-serif'}>
+            We invite you to{' '}
+            <Link href="/explore-platform">
+              <a style={{ color: '#1976d2', textDecoration: 'none' }}>explore our platform</a>
+            </Link>
+            , learn about the{' '}
+            <Link href="/customer-successes">
+              <a style={{ color: '#1976d2', textDecoration: 'none' }}>successes of our customers</a>
+            </Link>
+            , and{' '}
+            <Link href="/contact">
+              <a style={{ color: '#1976d2', textDecoration: 'none' }}>contact us</a>
+            </Link>
+            {' '}to learn more. We are here to orchestrate the IT world. Let&apos;s do it together.
+          </Typography>
+        </Box>
+
+        <Box
+          width="100%" // Definindo uma largura fixa
+          height={535} // Definindo uma altura fixa
+          position="relative" // Importante para uso correto do Next Image sem 'fill'
+          display={'flex'}
+          mt={8}
+          mb={8}
+          alignItems={'center'}
+          justifyContent={'center'}
+        >
+          <Image
+            src={'/assets/about-us/sb-stonebranch-in-facts-opt.svg'}
+            alt="About Us Image"
+            width={960}
+            height={535} // Altura ajustada
+            style={{
+              objectFit: 'fill', // Garante que a imagem não estoure os limites
+            }}
+          />
+        </Box>
+      </Container>
+      <Box
+        position="relative"
+        left={0}
+        right={0}
+        width="100%"
+        display={'flex'}
+        height={248}
+        alignItems={'center'}
+        justifyContent={'center'}
+        flexDirection={'column'}
+        sx={{
+          background: `linear-gradient(
+            130deg,
+            #186cda 30%, 
+            #186cda 50%, 
+            #05b8d8 100%
+          )`,
+        }}
+      >
+        <Typography
+          variant="h4"
+          fontWeight={400}
+          mt={4}
+          align='center'
+          color='white'
+          fontFamily={fontFamilyBoldVar}
+        >
+          Our Values are Part of our DNA <br />
+          and a Commitment to Our Working Culture
+        </Typography>
+        <Typography
+          variant="h6"
+          fontWeight={400}
+          mt={3}
+          mb={4}
+          align='center'
+          color='white'
+          fontFamily={'sans-serif'}
+        >
+          Together, these values set the stage for our service and promises to our clients, as well as our relationships with one another.
+        </Typography>
+      </Box>
+      <Box
+          position="relative"
+          left={0}
+          right={0}
+          width="100%"
+      >
+        <img
+          src={'/assets/about-us/stonebranch_about.jpg'}
+          alt="About Us Image"
+          width="100%"
+          style={{
+            objectFit: 'fill', // Garante que a imagem não estoure os limites
+          }}
+        />
+      </Box>
+      <Box
+        position="relative"
+        left={0}
+        right={0}
+        width="100%"
+        display={'flex'}
+        alignItems={'center'}
+        justifyContent={'center'}
+        height={216}
+        flexDirection={'column'}
+        sx={{
+          background: `linear-gradient(
+            130deg,
+            #186cda 30%, 
+            #186cda 50%, 
+            #05b8d8 100%
+          )`,
+        }}
+      >
+        <Typography
+          variant="h4"
+          fontWeight={400}
+          mt={4}
+          align='center'
+          color='white'
+          fontFamily={fontFamilyBoldVar}
+        >
+          Stonebranch Leadership Team
+        </Typography>
+        <Typography
+          variant="h6"
+          fontWeight={400}
+          mb={4}
+          mt={4}
+          align='center'
+          color='white'
+          fontFamily={'sans-serif'}
+        >
+          The Stonebranch leadership team is comprised of savvy entrepreneurs and seasoned industry experts, bringing years of <br/> experience and innovation to an established company recognized by analysts for its vision and value leadership.
+        </Typography>
+      </Box>
+      <CardsGrid
+        data={leadershipTeamData}
+        isBiggerThanLarge={isBiggerThanLarge}
+      />
+      <Box
+        position="relative"
+        left={0}
+        right={0}
+        width="100%"
+        display={'flex'}
+        alignItems={'center'}
+        justifyContent={'center'}
+        height={216}
+        flexDirection={'column'}
+        sx={{
+          background: `linear-gradient(
+            130deg,
+            #186cda 30%, 
+            #186cda 50%, 
+            #05b8d8 100%
+          )`,
+        }}
+      >
+        <Typography
+          variant="h4"
+          fontWeight={400}
+          mt={4}
+          align='center'
+          color='white'
+          fontFamily={fontFamilyBoldVar}
+        >
+          Board of Directors
+        </Typography>
+        <Typography
+          variant="h6"
+          fontWeight={400}
+          mb={4}
+          mt={4}
+          align='center'
+          color='white'
+          fontFamily={'sans-serif'}
+        >
+         Stonebranch&apos;s board of directors have a wealth of experience across the information technology, finance,<br/> and business sectors.
+        </Typography>
+      </Box>
+      <CardsGrid
+        data={directorsData}
+        isBiggerThanLarge={isBiggerThanLarge}
+        props={{
+          size: {
+            xs: 12,
+            sm: 12,
+            md: 6,
+            lg: 4,
+          }
+        }}
+      />
+    </Box>
+  );
+}
---
+++ src/utils/about-us/cards-data.ts
@@ -0,0 +1,66 @@
+export type CardData = {
+  title: string;
+  subtitle: string;
+  content: string;
+}
+
+export const leadershipTeamData: CardData[] = [
+  {
+    title: 'Giuseppe Damiani',
+    subtitle: 'Chief Executive Officer',
+    content: 'Mr. Giuseppe Damiani, the Chief Executive Officer of Stonebranch, brings more than 20 years of executive experience leading high-growth companies. Prior to joining Stonebranch, Mr. Damiani co-founded Lexagon Group, where he worked with leading telecom, media, and tech companies in North America, Europe, and Asia. Mr. Damiani has previously held various roles within Stonebranch, including CFO and COO. Before Lexagon Group, he was a consultant with Booz, Allen & Hamilton and A.T. Kearney.'
+  },
+  {
+    title: 'Peter Baljet',
+    subtitle: 'Chief Technology Officer',
+    content: 'As the Chief Technology Officer, Peter Baljet’s mission is to bring Stonebranch’s vision to the market. With more than 25 years of experience in technology product strategy, development, and operations, he is a recognized industry thought leader in the areas of Cloud and Big Data/Analytics arenas. Prior to Stonebranch, Mr. Baljet helped drive innovations as a senior leader with organizations that include SAP, Deloitte Consulting, and Visiprise. He holds a bachelor’s degree in Computer Engineering from Auburn University and a master’s degree (MBA) in Management of Technology from the Georgia Institute of Technology.'
+  },
+  {
+    title: 'Marcos Medina',
+    subtitle: 'Chief Sales Officer',
+    content: "As the Chief Sales Officer, Mr. Medina is responsible for Stonebranch's strategic sales and partnership initiatives. He leads the global sales team in developing new business opportunities, creating customized solutions for customers, and collaboratively forging a strategic path for the Stonebranch vision. Prior to joining Stonebranch, Mr. Medina spent nearly 15 years working in senior-level roles for organizations including Ericsson and LHS."
+  },
+  {
+    title: "Ray Pacleb",
+    subtitle: "Chief Financial Officer",
+    content: "As the Chief Financial Officer, Mr. Pacleb oversees Stonebranch’s global financial planning, accounting, reporting, analysis, business analytics, tax and treasury, and financial administrative operations. He has more than 20 years of experience as a financial strategic leader. Mr. Pacleb joined Stonebranch in 2014. He previously worked with global businesses including the Coca-Cola Company and Mizuno Corp. in finance and accounting leadership roles. Mr. Pacleb is a Certified Public Accountant with a bachelor’s degree in Accounting and a master’s degree in Financial Management."
+  },
+  {
+    title: "Scott Davis",
+    subtitle: "Chief Marketing Officer",
+    content: "As the Chief Marketing Officer, Mr. Davis is an experienced revenue marketing leader who is focused on the corporate go-to-market strategy. He is responsible for Stonebranch’s global brand, product marketing, corporate communications, and demand generation. Mr. Davis can be found as a thought leader on the topics of marketing technology, BI/analytics and IT automation. Prior to Stonebranch, Mr. Davis held senior leadership roles with SAP and Calero Software. He graduated from the University of South Carolina with a Bachelor of Science in Business Administration."
+  },
+  {
+    title: "Sebastiano Mion",
+    subtitle: "Chief Partner Officer",
+    content: "As the Chief Partner Officer, Mr. Mion is responsible for Stonebranch’s global partner management strategy, including strategic partner sales and partnership initiatives. In 2010, he co-founded Daring Star Ltd, which built a solid reputation for integrating large, complex IT projects for telecommunications operators throughout Europe and Africa. Mr. Mion joined Stonebranch after the merger with Daring Star in 2017. Mr. Mion holds a bachelor’s degree in Statistics and Economics from the University of Venice (Italy)."
+  },
+  {
+    title: "Nicole Spartis",
+    subtitle: "Chief People Officer",
+    content: "As the Chief People Officer, Ms. Spartis is responsible for Stonebranch’s overall HR strategy, including recruitment and retention, company culture, organizational development, and communications. She has expertise in building successful HR departments at fast-growing global technology companies. Prior to Stonebranch, Ms. Spartis held senior leadership roles with Fiserv, EarthLink, and ClickDimensions. She holds an SPHR Certification (Senior Professional in Human Resources) and has a bachelor of science degree in Management from Georgia Tech."
+  },
+  {
+    title: "Paulo Naddeo",
+    subtitle: "Chief Delivery Officer",
+    content: "As the Chief Delivery Officer, Mr. Naddeo is responsible for Stonebranch's global delivery. He is responsible for the organization's delivery framework, project management practice, resource management, and sales support. Mr. Naddeo comes from an intensive delivery background having provided IT services to large telecommunication companies in the US and around the world. He has expertise on building solid delivery teams, structuring the work around well-defined methods and practices, and in managing customers during the delivery process. Mr. Naddeo holds a bachelor’s degree in electrical engineering and a master’s degree in Computer Science from the University of São Paulo (Brazil)."
+  },
+]
+
+export const directorsData: CardData[] = [
+  {
+    title: "Amit Shah",
+    subtitle: "Chairman of the Board",
+    content: "Mr. Shah has spent his career investing in, starting, running, and advising software companies on product strategy, pricing, and go-to-market strategies. He was the founder and CEO of Jirafe, which was acquired by SAP Hybris. Prior to that, Mr. Shah was VP of Sales & Business Development for Magento, which was acquired by Adobe. Today he serves on the boards of Magnolia International Ltd, Doodle AG, and ROQQIO GmbH."
+  },
+  {
+    title: "Andreas Resch",
+    subtitle: "Member of the Board",
+    content: "An educator and industry leader, Mr. Resch has held organizational, IT, and logistics roles in a wide range of business sectors — most recently as CIO at Bayer AG and partner at Modalis Management. His work has been documented in a book co-written with Prof. Brenner, which earned him inclusion in the CIO Hall of Fame in the United States. Today he holds several positions as a supervisory board member and works in the fields of management and IT consulting."
+  },
+  {
+    title: "Martin Berchtenbreiter",
+    subtitle: "Member of the Board",
+    content: "Mr. Berchtenbreiter began his career at Microsoft in 1996, spending nearly 20 years in various sales, marketing, and general management roles. In his last position as General Manager Mittelstand & Partner, he was responsible for B2B software and services, including ERP/CRM businesses. Subsequently, as Chief Commercial Officer at 1&1 Versatel GmbH (part of United Internet AG), he led the company's broadband expansion using glass fiber optic technology. Additionally, he chaired the advisory board for SelectLine GmbH. Today he serves as an executive advisor for several IT companies and sits on the Dokufy GmbH advisory board."
+  },
+]
